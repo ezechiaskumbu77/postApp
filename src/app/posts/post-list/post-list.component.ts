4b772334df1a8ebeb7b2e56f82bc3202893d03b3
Ezechias Kumbu Kumbu <ezechiaskumbu77@gmail.com>
2022-02-03 13:01:17 +0100

    feeding post-list with fdynamic component, and restilying post create component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,17 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-list.component.scss']
 })
 export class PostListComponent implements OnInit {
-posts = [
-  {
-    title: 'First post', content: 'This is the first post\'s content'
-  },
-  {
-    title: 'Secoond post', content: 'This is the second post\'s content'
-  },
-  {
-    title: 'Third post', content: 'This is the third post\'s content'
-  }
-]
+  // posts = [
+  //   {
+  //     title: 'First post', content: 'This is the first post\'s content'
+  //   },
+  //   {
+  //     title: 'Secoond post', content: 'This is the second post\'s content'
+  //   },
+  //   {
+  //     title: 'Third post', content: 'This is the third post\'s content'
+  //   }
+  // ]
+  posts = [];
   constructor() { }
 
   ngOnInit(): void {
